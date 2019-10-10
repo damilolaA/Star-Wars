e7baa36f851bb6dbdcc1710687c8db7d3b303d0b
damilolaa <oadio@tradedepot.co>
2019-10-10 09:30:48 +0100

    refactor to use react-select

--- src/components/GenderFilter/index.js
+++ src/components/GenderFilter/index.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import { ALL, FEMALE, MALE, HERMAPHRODITE, NA } from '../../utils/constants';
+import Select from 'react-select';
 
+import './index.scss';
+
+const options = [
+  { value: 'all', label: 'All' },
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "hermaphrodite", label: "Hermaphrodite" },
+  { value: "n/a", label: "N/A" },
+];
 
 const GenderFilter = ({ handleFilter, filterName }) => {
   return(
     <div>
-      <FormControl>
-        <InputLabel htmlFor="movies">Filter By Gender</InputLabel>
-        <Select
-          value={filterName}
-          onChange={(e) => handleFilter(e)}
-        >
-          <MenuItem value={ALL}>
-            All
-          </MenuItem>
-          <MenuItem value={MALE}>
-            Male
-          </MenuItem>
-          <MenuItem value={FEMALE}>
-            Female
-          </MenuItem>
-          <MenuItem value={HERMAPHRODITE}>
-            Hermaphrodite
-          </MenuItem>
-          <MenuItem value={NA}>
-            N/A
-          </MenuItem>
-        </Select>
-      </FormControl>
+      <Select 
+        className="filter-dropdown"
+        onChange={filter => handleFilter(filter)}
+        placeholder= "Filter"
+        options={options}
+        autoFocus={true}
+      />
     </div>
   )
 }
